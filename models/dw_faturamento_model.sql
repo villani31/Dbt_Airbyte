0d1cfa688544215db28e2b2f69b717be8fd33f5a
root <iamthiagovillani@gmail.com>
2022-09-19 06:22:52 -0300

    segunda alteracao

--- models/dw_faturamento_model.sql
+++ models/dw_faturamento_model.sql
@@ -1,7 +1,7 @@
 with dw_faturamento_model as (
     SELECT pro.model as "Modelo"
         ,sum(pro.price) as "Faturamento"
-    from public.airbyte_purchases pur INNER JOIN airbyt_products pro
+    from public.airbyte_purchases pur INNER JOIN airbyte_products pro
     on pur.product_id = pro.id
     GROUP BY pro.model
     ORDER BY 2 DESC
